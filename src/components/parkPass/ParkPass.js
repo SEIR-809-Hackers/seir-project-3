6c9063c08d7946bf85da11449b5078d6636cab4b
Pablo Acebedo <pablofelipe@me.com>
2021-10-12 12:04:13 -0500

    Styling Pablo & Kaleb

--- src/components/parkPass/ParkPass.js
+++ src/components/parkPass/ParkPass.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ParkPassList from './ParkPassList';
 import { useContext } from 'react';
 import { DataContext } from '../../DataContext';
+import hiking from '../../assets/9831.jpg';
+import './ParkPass.css'
 
 function ParkPass() {
     const { currentUser, setCurrentUser } = useContext(DataContext);
     return (
-        <div>
-            <h1>Hello</h1>
-            <h2>{currentUser.username}</h2>
+        <div className='container'>
+            <div className='welcome'>
+                <h1>Hello,</h1>    
+                <h2>{currentUser.username}</h2>
+            </div>
             <ParkPassList />
+            <div className="divide">
+            <h3>Get Hiking!</h3>
+            <img className="hiking" src={hiking} alt="People Hiking"></img> 
+            </div> 
         </div>
     );
 }
--- src/components/parkPass/ParkPassItem.js
+++ src/components/parkPass/ParkPassItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import MainList from '../styles/list';
 import { useContext } from 'react';
 import { DataContext } from '../../DataContext';
+import './ParkPass.css'
+import hiked from '../../assets/hikedstamp.png'
+import nothiked from '../../assets/NotHiked.png'
 
 
 function ParkPassItem({user}) {
@@ -10,20 +12,24 @@ function ParkPassItem({user}) {
     let parks = currentUser.myParks
 
     return (
-			<MainList>
+			<div className="list-select-parks">
+				<h2>My Parks:</h2>
 				{parks.length ? 
 				parks.map((select) => (
 					<Link
 						to={'/parks/' + select.park._id}
-						style={{ textDecoration: 'none' }}>
-						<p>{select.park.parkName}</p>
-                        {select.seen ? <p>(hiked)🥾</p> : <p>(want to see)🏔</p>}
+						style={{ textDecoration: 'none', color: "white" }}>
+						<div className="each-park">	
+						<h4>{select.park.parkName}</h4>
+                        {select.seen ? <img className='stamp'src={hiked}/> : <img src={nothiked} className='stamp'/>}
+						<button className="delete-park">❌</button>
+						</div>
 					</Link>
 				)) :
 				<h2>Add Parks</h2>
 				}
 				{}
-			</MainList>
+			</div>
 		);
 }
 
